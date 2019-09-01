8ba82b65350396075723b8a7b9a18227c216f457
pgdien <pgdien2@gmail.com>
2019-09-01 09:45:47 +0700

    add

---
+++ BanHang_API/Connect/CT_DonHang_DTO.cs
@@ -0,0 +1,154 @@
+﻿using BanHang_API.Model;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace BanHang_API.Connect
+{
+    public class CT_DonHang_DTO
+    {
+        public List<CT_DonHang> getCT_DonHang()
+        {
+            List<CT_DonHang> lCT_DonHang = new List<CT_DonHang>();
+            using (MySqlConnection connMySQL = new MySqlConnection(Conn.connString))
+            {
+                using (MySqlCommand cmd = connMySQL.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT CTDH_ID, CTDH_CHA_ID, DONHANG_ID, HANGHOA_ID, DONGIA, SOLUONG, TONGTIEN, THUCTHU, TIEN_CONGTHEM, GHICHU FROM CHITIET_DH";
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Connection = connMySQL;
+                    connMySQL.Open();
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            lCT_DonHang.Add(new CT_DonHang
+                            {
+                                CTDH_ID = reader.GetInt32(reader.GetOrdinal("CTDH_ID")),
+                                CTDH_CHA_ID = reader.IsDBNull(reader.GetOrdinal("CTDH_CHA_ID")) ? 0 : reader.GetInt32(reader.GetOrdinal("CTDH_CHA_ID")),
+                                DONHANG_ID = reader.GetInt32(reader.GetOrdinal("DONHANG_ID")),
+                                HANGHOA_ID = reader.GetInt32(reader.GetOrdinal("HANGHOA_ID")),
+                                DONGIA = reader.GetFloat(reader.GetOrdinal("DONGIA")),
+                                SOLUONG = reader.GetFloat(reader.GetOrdinal("SOLUONG")),
+                                TONGTIEN = reader.GetFloat(reader.GetOrdinal("TONGTIEN")),
+                                THUCTHU = reader.GetFloat(reader.GetOrdinal("THUCTHU")),
+                                TIEN_CONGTHEM = reader.GetFloat(reader.GetOrdinal("TIEN_CONGTHEM")),
+                                GHICHU = reader.GetString(reader.GetOrdinal("GHICHU"))
+                            });
+                        }
+                    }
+                }
+                connMySQL.Close();
+            }
+            return lCT_DonHang;
+        }
+        public CT_DonHang getCT_DonHang(int id)
+        {
+            CT_DonHang lCT_DonHang = new CT_DonHang();
+            using (MySqlConnection connMySQL = new MySqlConnection(Conn.connString))
+            {
+                using (MySqlCommand cmd = connMySQL.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT CTDH_ID, CTDH_CHA_ID, DONHANG_ID, HANGHOA_ID, DONGIA, SOLUONG, TONGTIEN, THUCTHU, TIEN_CONGTHEM, GHICHU FROM CHITIET_DH where CTDH_ID=@CTDH_ID";
+                    cmd.Parameters.Add(new MySqlParameter("CTDH_ID", id));
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Connection = connMySQL;
+                    connMySQL.Open();
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            lCT_DonHang = (new CT_DonHang
+                            {
+                                CTDH_ID = reader.GetInt32(reader.GetOrdinal("CTDH_ID")),
+                                CTDH_CHA_ID = reader.IsDBNull(reader.GetOrdinal("CTDH_CHA_ID")) ? 0 : reader.GetInt32(reader.GetOrdinal("CTDH_CHA_ID")),
+                                DONHANG_ID = reader.GetInt32(reader.GetOrdinal("DONHANG_ID")),
+                                HANGHOA_ID = reader.GetInt32(reader.GetOrdinal("HANGHOA_ID")),
+                                DONGIA = reader.GetFloat(reader.GetOrdinal("DONGIA")),
+                                SOLUONG = reader.GetFloat(reader.GetOrdinal("SOLUONG")),
+                                TONGTIEN = reader.GetFloat(reader.GetOrdinal("TONGTIEN")),
+                                THUCTHU = reader.GetFloat(reader.GetOrdinal("THUCTHU")),
+                                TIEN_CONGTHEM = reader.GetFloat(reader.GetOrdinal("TIEN_CONGTHEM")),
+                                GHICHU = reader.GetString(reader.GetOrdinal("GHICHU"))
+                            });
+                        }
+                    }
+                }
+                connMySQL.Close();
+            }
+            return lCT_DonHang;
+        }
+        public int addCT_DonHang(CT_DonHang ct_DH)
+        {
+            int kq;
+            using (MySqlConnection connMySQL = new MySqlConnection(Conn.connString))
+            {
+                using (MySqlCommand cmd = connMySQL.CreateCommand())
+                {
+                    cmd.CommandText = "INSERT INTO CHITIET_DH(CTDH_CHA_ID, DONHANG_ID, HANGHOA_ID, DONGIA, SOLUONG, TONGTIEN, THUCTHU, TIEN_CONGTHEM, GHICHU) V" +
+                                                    "ALUES (@CTDH_CHA_ID, @DONHANG_ID, @HANGHOA_ID, @DONGIA, @SOLUONG, @TONGTIEN, @THUCTHU, @TIEN_CONGTHEM, @GHICHU)";
+                    cmd.Parameters.Add(new MySqlParameter("CTDH_CHA_ID", ct_DH.CTDH_CHA_ID));
+                    cmd.Parameters.Add(new MySqlParameter("DONHANG_ID", ct_DH.DONHANG_ID));
+                    cmd.Parameters.Add(new MySqlParameter("HANGHOA_ID", ct_DH.HANGHOA_ID));
+                    cmd.Parameters.Add(new MySqlParameter("DONGIA", ct_DH.DONGIA));
+                    cmd.Parameters.Add(new MySqlParameter("SOLUONG", ct_DH.SOLUONG));
+                    cmd.Parameters.Add(new MySqlParameter("TONGTIEN", ct_DH.TONGTIEN));
+                    cmd.Parameters.Add(new MySqlParameter("THUCTHU", ct_DH.THUCTHU));
+                    cmd.Parameters.Add(new MySqlParameter("TIEN_CONGTHEM", ct_DH.TIEN_CONGTHEM));
+                    cmd.Parameters.Add(new MySqlParameter("GHICHU", ct_DH.GHICHU));
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Connection = connMySQL;
+                    connMySQL.Open();
+                    kq = cmd.ExecuteNonQuery();
+                }
+                connMySQL.Close();
+            }
+            return kq;
+        }
+        public int editCT_DonHang(CT_DonHang ct_DH)
+        {
+            int kq;
+            using (MySqlConnection connMySQL = new MySqlConnection(Conn.connString))
+            {
+                using (MySqlCommand cmd = connMySQL.CreateCommand())
+                {
+                    cmd.CommandText = "UPDATE CHITIET_DH SET CTDH_CHA_ID=@CTDH_CHA_ID,DONHANG_ID=@DONHANG_ID,HANGHOA_ID=@HANGHOA_ID,DONGIA=@DONGIA,SOLUONG=@SOLUONG," +
+                        "TONGTIEN=@TONGTIEN,THUCTHU=@THUCTHU,TIEN_CONGTHEM=@TIEN_CONGTHEM,GHICHU=@GHICHU WHERE CTDH_ID=@CTDH_ID";
+                    cmd.Parameters.Add(new MySqlParameter("CTDH_CHA_ID", ct_DH.CTDH_CHA_ID));
+                    cmd.Parameters.Add(new MySqlParameter("DONHANG_ID", ct_DH.DONHANG_ID));
+                    cmd.Parameters.Add(new MySqlParameter("HANGHOA_ID", ct_DH.HANGHOA_ID));
+                    cmd.Parameters.Add(new MySqlParameter("DONGIA", ct_DH.DONGIA));
+                    cmd.Parameters.Add(new MySqlParameter("SOLUONG", ct_DH.SOLUONG));
+                    cmd.Parameters.Add(new MySqlParameter("TONGTIEN", ct_DH.TONGTIEN));
+                    cmd.Parameters.Add(new MySqlParameter("THUCTHU", ct_DH.THUCTHU));
+                    cmd.Parameters.Add(new MySqlParameter("TIEN_CONGTHEM", ct_DH.TIEN_CONGTHEM));
+                    cmd.Parameters.Add(new MySqlParameter("GHICHU", ct_DH.GHICHU));
+                    cmd.Parameters.Add(new MySqlParameter("CTDH_ID", ct_DH.CTDH_ID));
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Connection = connMySQL;
+                    connMySQL.Open();
+                    kq = cmd.ExecuteNonQuery();
+                }
+                connMySQL.Close();
+            }
+            return kq;
+        }
+        public int delCT_DonHang(int id)
+        {
+            int kq;
+            using (MySqlConnection connMySQL = new MySqlConnection(Conn.connString))
+            {
+                using (MySqlCommand cmd = connMySQL.CreateCommand())
+                {
+                    cmd.CommandText = "DELETE FROM CHITIET_DH WHERE CTDH_ID=@CTDH_ID";
+                    cmd.Parameters.Add(new MySqlParameter("CTDH_ID", id));
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Connection = connMySQL;
+                    connMySQL.Open();
+                    kq = cmd.ExecuteNonQuery();
+                }
+                connMySQL.Close();
+            }
+            return kq;
+        }
+    }
+}
---
+++ BanHang_API/Connect/DonHang_DTO.cs
@@ -0,0 +1,143 @@
+﻿using BanHang_API.Model;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace BanHang_API.Connect
+{
+    public class DonHang_DTO
+    {
+        public List<DonHang> getDonHang()
+        {
+            List<DonHang> lDonHang = new List<DonHang>();
+            using (MySqlConnection connMySQL = new MySqlConnection(Conn.connString))
+            {
+                using (MySqlCommand cmd = connMySQL.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT DONHANG_ID, KHACHHANG_ID, NGAY_LAP, LOAIDH_ID, TTDH_ID, MA_DH, STT FROM DONHANG";
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Connection = connMySQL;
+                    connMySQL.Open();
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            lDonHang.Add(new DonHang
+                            {
+                                DONHANG_ID = reader.GetInt32(reader.GetOrdinal("DONHANG_ID")),
+                                KHACHHANG_ID = reader.IsDBNull(reader.GetOrdinal("KHACHHANG_ID")) ? 0 : reader.GetInt32(reader.GetOrdinal("KHACHHANG_ID")),
+                                NGAY_LAP = reader.GetDateTime(reader.GetOrdinal("NGAY_LAP")),
+                                LOAIDH_ID = reader.GetInt32(reader.GetOrdinal("LOAIDH_ID")),
+                                TTDH_ID = reader.GetInt32(reader.GetOrdinal("TTDH_ID")),
+                                MA_DH = reader.GetString(reader.GetOrdinal("MA_DH")),
+                                STT = reader.GetInt32(reader.GetOrdinal("STT"))
+                            });
+                        }
+                    }
+                }
+                connMySQL.Close();
+            }
+            return lDonHang;
+        }
+        public DonHang getDonHang(int id)
+        {
+            DonHang lDonHang = new DonHang();
+            using (MySqlConnection connMySQL = new MySqlConnection(Conn.connString))
+            {
+                using (MySqlCommand cmd = connMySQL.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT DONHANG_ID, KHACHHANG_ID, NGAY_LAP, LOAIDH_ID, TTDH_ID, MA_DH, STT FROM DONHANG where DONHANG_ID=@DONHANG_ID";
+                    cmd.Parameters.Add(new MySqlParameter("DONHANG_ID", id));
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Connection = connMySQL;
+                    connMySQL.Open();
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            lDonHang = (new DonHang
+                            {
+                                DONHANG_ID = reader.GetInt32(reader.GetOrdinal("DONHANG_ID")),
+                                KHACHHANG_ID = reader.IsDBNull(reader.GetOrdinal("KHACHHANG_ID")) ? 0 : reader.GetInt32(reader.GetOrdinal("KHACHHANG_ID")),
+                                NGAY_LAP = reader.GetDateTime(reader.GetOrdinal("NGAY_LAP")),
+                                LOAIDH_ID = reader.GetInt32(reader.GetOrdinal("LOAIDH_ID")),
+                                TTDH_ID = reader.GetInt32(reader.GetOrdinal("TTDH_ID")),
+                                MA_DH = reader.GetString(reader.GetOrdinal("MA_DH")),
+                                STT = reader.GetInt32(reader.GetOrdinal("STT"))
+                            });
+                        }
+                    }
+                }
+                connMySQL.Close();
+            }
+            return lDonHang;
+        }
+        public int addDonHang(DonHang DH)
+        {
+            int kq;
+            using (MySqlConnection connMySQL = new MySqlConnection(Conn.connString))
+            {
+                using (MySqlCommand cmd = connMySQL.CreateCommand())
+                {
+                    cmd.CommandText = "INSERT INTO DONHANG(KHACHHANG_ID, NGAY_LAP, LOAIDH_ID, TTDH_ID, MA_DH, STT, GHICHU) " +
+                                                    "VALUES (@KHACHHANG_ID, @NGAY_LAP, @LOAIDH_ID, @TTDH_ID, @MA_DH, @STT, @GHICHU)";
+                    cmd.Parameters.Add(new MySqlParameter("KHACHHANG_ID", DH.KHACHHANG_ID));
+                    cmd.Parameters.Add(new MySqlParameter("NGAY_LAP", DH.NGAY_LAP));
+                    cmd.Parameters.Add(new MySqlParameter("LOAIDH_ID", DH.LOAIDH_ID));
+                    cmd.Parameters.Add(new MySqlParameter("TTDH_ID", DH.TTDH_ID));
+                    cmd.Parameters.Add(new MySqlParameter("MA_DH", DH.MA_DH));
+                    cmd.Parameters.Add(new MySqlParameter("STT", DH.STT));
+                    cmd.Parameters.Add(new MySqlParameter("GHICHU", DH.GHICHU));
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Connection = connMySQL;
+                    connMySQL.Open();
+                    kq = cmd.ExecuteNonQuery();
+                }
+                connMySQL.Close();
+            }
+            return kq;
+        }
+        public int editDonHang(DonHang DH)
+        {
+            int kq;
+            using (MySqlConnection connMySQL = new MySqlConnection(Conn.connString))
+            {
+                using (MySqlCommand cmd = connMySQL.CreateCommand())
+                {
+                    cmd.CommandText = "UPDATE DONHANG SET KHACHHANG_ID=@KHACHHANG_ID,NGAY_LAP=@NGAY_LAP,LOAIDH_ID=@LOAIDH_ID,TTDH_ID=@TTDH_ID,MA_DH=@MA_DH,STT=@STT,GHICHU=@GHICHU WHERE DONHANG_ID=@DONHANG_ID";
+                    cmd.Parameters.Add(new MySqlParameter("KHACHHANG_ID", DH.KHACHHANG_ID));
+                    cmd.Parameters.Add(new MySqlParameter("NGAY_LAP", DH.NGAY_LAP));
+                    cmd.Parameters.Add(new MySqlParameter("LOAIDH_ID", DH.LOAIDH_ID));
+                    cmd.Parameters.Add(new MySqlParameter("TTDH_ID", DH.TTDH_ID));
+                    cmd.Parameters.Add(new MySqlParameter("MA_DH", DH.MA_DH));
+                    cmd.Parameters.Add(new MySqlParameter("STT", DH.STT));
+                    cmd.Parameters.Add(new MySqlParameter("GHICHU", DH.GHICHU));
+                    cmd.Parameters.Add(new MySqlParameter("DONHANG_ID", DH.DONHANG_ID));
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Connection = connMySQL;
+                    connMySQL.Open();
+                    kq = cmd.ExecuteNonQuery();
+                }
+                connMySQL.Close();
+            }
+            return kq;
+        }
+        public int delDonHang(int id)
+        {
+            int kq;
+            using (MySqlConnection connMySQL = new MySqlConnection(Conn.connString))
+            {
+                using (MySqlCommand cmd = connMySQL.CreateCommand())
+                {
+                    cmd.CommandText = "DELETE FROM DONHANG WHERE DONHANG_ID=@DONHANG_ID";
+                    cmd.Parameters.Add(new MySqlParameter("DONHANG_ID", id));
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Connection = connMySQL;
+                    connMySQL.Open();
+                    kq = cmd.ExecuteNonQuery();
+                }
+                connMySQL.Close();
+            }
+            return kq;
+        }
+    }
+}
